682a44cb3b369e72a4134b98342c4551e2b7d59d
Krishna Saxena <krishnasaxena@gmail.com>
2026-02-08 00:37:33 +0530

    Solved Problems

---
+++ src/com/gla/strings/LC58.java
@@ -0,0 +1,10 @@
+package com.gla.strings;
+
+public class LC58 {
+    public int lengthOfLastWord(String s) {
+        String[] arr= s.split(" ");
+        String lastWord=arr[arr.length-1];
+        return lastWord.length();
+    }
+}
+
